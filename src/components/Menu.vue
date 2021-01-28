0842ec8759e89dbacf24cea5769c9be7f6e0c92f
Geice Gomes <sogmesmo@gmail.com>
2021-01-28 13:33:08 -0300

    Criaçao do Menu e Navbar

---
+++ src/components/Menu.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <Navbar/>
+        
+    </header>
+</template>
+ 
+ 
+<script>
+
+
+import Navbar from './Navbar.vue';
+
+export default {
+    name: 'menu',
+    components: {
+        Navbar      
+    }   
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+header {
+    width: 100vw;
+    background-color:#17222D ;
+    padding: 15px;
+}
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <nav>
+      <div class="menu-item"><a href="#">Lista</a></div>
+      <div class="menu-item"><a href="#">Cadastro</a></div> 
+    </nav>
+</template>
+
+<script>
+
+
+export default {
+    name: 'navbar',
+    data () {
+        return
+    }
+}
+</script>
+
+<style scoped>
+ nav {
+     display: flex;
+     align-items: center;
+     justify-content:flex-end;
+ }
+ nav .menu-item {
+     color: #fff;
+     padding: 10px 20px;
+     position: relative;
+     text-align: center;
+     border-bottom: 3px solid transparent;
+     display: flex;
+     transition: 0.4s;
+
+ }
+ nav .menu-item.active,
+ nav .menu-item:hover{
+     background-color:#445566 ;
+     border-bottom-color: #EFF4F9;
+ }
+
+ nav .menu-item a {
+    
+     color : inherit;
+     text-decoration: none;
+     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+ }
+</style>
+
